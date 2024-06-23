09c1ddfa5aa1813d886f4f861d3d6469e2984bb9
Randima-Silva <randimadevelopment@gmail.com>
2024-06-23 18:54:57 +0530

    deleted

--- src/util/index.tsx
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateString: string): string => {
-    const [day, month, year] = dateString.split('/');
-    const date = new Date(`${year}-${month}-${day}`);
-    const options: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-  };
-  
